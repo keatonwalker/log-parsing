a34a4f3eac7acd4b77ac46023bdf94be1bef687a
keatonwalker <keatwalker@gmail.com>
2017-02-13 11:05:53 -0700

    Parsing implemented

--- forklift-log-parse.py
+++ forklift-log-parse.py
@@ -1,83 +1,149 @@
 import re
 import os
+import csv
+from time import strftime, strptime
 
+date_time_run = strftime("%Y%m%d")  # _%H%M%S")
 
 time_matcher = re.compile(r'(\d{2}:\d{2}:\d{2})')
-name_matcher = re.compile(r"destination': '([^']+)")
+crate_matcher = re.compile(r"destination': '([^']+)")
 coord_sys_matcher = re.compile(r"destination_coordinate_system': u'([^']+)")
 pallet_matcher = re.compile(r"lift:   39 processing crates for pallet: ([^\r]+)")
 
 
+def seconds_from_one(time_string):
+    t = strptime(time_string, "%H:%M:%S")
+    seconds_from_1 = 0
+    seconds_from_1 += (t.tm_hour - 1) * 3600
+    seconds_from_1 += t.tm_min * 60
+    seconds_from_1 += t.tm_sec
+    return seconds_from_1
+
+
 class CrateParser(object):
+    '''
+        Common data and utilities for crate pasring types
+    '''
+    match_functions = {}  # match string: line parse functions
+    store_functions = {}  # class name: store functions for end of a parse
 
     def __init__(self):
-        self.re_funcs = {}
-
         self.pallet = None
-        self.add_match_function('lift:   39', self.set_pallet)
+        self.add_match_function('processing crates for pallet:', self.set_pallet)
 
-        self.name = None
-        self.add_match_function('lift:   56', self.set_name)
+        self.crate = None
+        self.add_match_function("{   'destination': '", self.set_crate)
 
         self.destination_coord_sys = None
         self.add_match_function('destination_coordinate_system', self.set_destination_coord_sys)
 
-        self.matcher = None
         self.records = []
 
     def add_match_function(self, match_string, function):
-        funcs = self.re_funcs.get(match_string, [])
+        '''
+        match_string: string to match log line
+        function: function(line:string): store function | None
+        '''
+        funcs = CrateParser.match_functions.get(match_string, [])
         funcs.append(function)
-        self.re_funcs[match_string] = funcs
-
-    def call_match_functions(self, match_string, line):
-        for f in self.re_funcs[match_string]:
-            f(line)
+        CrateParser.match_functions[match_string] = funcs
 
     def set_pallet(self, line):
+        '''
+            Line: line containing pallet name
+        '''
+        self.reset_fields()
         self.pallet = pallet_matcher.search(line).group(1)
 
-    def set_name(self, line):
+    def set_crate(self, line):
+        '''
+            Line: line containing crate name
+        '''
         self.reset_fields()
-        name = name_matcher.search(line).group(1)
-        self.name = name
+        crate = crate_matcher.search(line).group(1)
+        self.crate = crate
 
     def set_destination_coord_sys(self, line):
+        '''
+            Line: line containing destination coordinate system
+        '''
         self.destination_coord_sys = coord_sys_matcher.search(line).group(1)
 
-    def get_re(self):
-        group_sep = r'|'
-        re_string = group_sep.join(self.re_funcs.keys())
-        re_string = group_sep.join('(%s)' % s for s in self.re_funcs.keys())
-        return re_string
-
     def store_record(self):
-        self.records.append(str(self))
+        '''
+            Store fields for output and reset them.
+        '''
+        self.records.append(self.get_record())
         self.reset_fields()
 
     def reset_fields(self):
+        """
+            reset all fields for the type
+
+            implemented in subclasses"""
         pass
+
+    @staticmethod
+    def call_match_functions(match_string, line):
+        '''
+            match_string: string
+                String that matched the line. Key to get match function
+            line: string
+                matched line
+        '''
+        for f in CrateParser.match_functions[match_string]:
+            #: record storing function returned if line triggers a store
+            store_func = f(line)
+            if store_func:
+                #: Keep store function so it can be called after all parse functions for current line
+                CrateParser.store_functions[store_func.im_class] = store_func
+
+    @staticmethod
+    def get_search_strings():
+        '''
+            return: list[string]
+                list of match strings each line is tested against
+        '''
+        return CrateParser.match_functions.keys()
+
+    @staticmethod
+    def store_records():
+        '''
+            Store records for type that got triggered by a line
+        '''
+        for f in CrateParser.store_functions.values():
+            if f:
+                f()
+        CrateParser.store_functions = {}
 
 
 class HashInsertTemp(CrateParser):
 
     def __init__(self):
         super(HashInsertTemp, self).__init__()
-        # self.name = 'test'
-        self.start = 'core:  237'
+
+        self.output_filename = 'HashAndInsertTemp_{}.csv'.format(date_time_run)
+        self.output_fields = [
+            'pallet',
+            'dest',
+            'dest_coord_sys',
+            'start',
+            'end',
+            'duration',
+            'added']
+
+        self.start = 'checking for changes...'
         self.add_match_function(self.start, self.start_parse)
         self.start_time = None
 
-        self.adds = 'core:  147'
+        self.adds = 'Number of rows to be added:'
         self.adds_matcher = re.compile(r'added: (\d+)')
         self.add_match_function(self.adds, self.set_adds)
         self.add_number = None
-        # add reset method at the end
-        self.stop = 'core:  147'
+
+        self.stop = 'Number of rows to be added:'
         self.add_match_function(self.stop, self.end_parse)
         self.stop_time = None
-
-        self.matcher = re.compile(self.get_re())
 
     def start_parse(self, line):
         time = time_matcher.search(line).group()
@@ -86,24 +152,35 @@ class HashInsertTemp(CrateParser):
     def end_parse(self, line):
         time = time_matcher.search(line).group()
         self.stop_time = time
-        self.store_record()
+        return self.store_record
 
     def set_adds(self, line):
         self.add_number = self.adds_matcher.search(line).group(1)
 
     def reset_fields(self):
-        self.name = None
+        self.crate = None
         self.start_time = None
         self.stop_time = None
         self.add_number = None
 
-    def __str__(self):
-        return '{},{},{},{},{},{}'.format(
+    def get_record(self):
+        return (
             self.pallet,
-            self.name,
+            self.crate,
             self.destination_coord_sys,
             self.start_time,
             self.stop_time,
+            seconds_from_one(self.stop_time) - seconds_from_one(self.start_time),
+            self.add_number)
+
+    def __str__(self):
+        return '{},{},{},{},{},{},{}'.format(
+            self.pallet,
+            self.crate,
+            self.destination_coord_sys,
+            self.start_time,
+            self.stop_time,
+            seconds_from_one(self.stop_time) - seconds_from_one(self.start_time),
             self.add_number)
 
 
@@ -111,57 +188,84 @@ class Reproject(CrateParser):
 
     def __init__(self):
         super(Reproject, self).__init__()
-        #self.name = 'test'
-        self.start = 'core:  237'
-        self.re_funcs[self.start] = self.start_parse
-        self.start_time = None
-        self.stop = 'core:  147'
-        self.re_funcs[self.stop] = self.end_parse
-        self.stop_time = None
 
-        self.matcher = re.compile(self.get_re())
+        self.output_filename = 'Reproject_{}.csv'.format(date_time_run)
+        self.output_fields = [
+            'pallet',
+            'dest',
+            'dest_coord_sys',
+            'start',
+            'end',
+            'duration',
+            'added']
+
+        self.start = 'Number of rows to be added'
+        self.add_match_function(self.start, self.start_parse)
+        self.start_time = None
+
+        self.adds = 'Number of rows to be added'
+        self.adds_matcher = re.compile(r'added: (\d+)')
+        self.add_match_function(self.adds, self.set_adds)
+        self.add_number = None
+
+        self.stop = 'starting edit session...'
+        self.add_match_function(self.stop, self.end_parse)
+        self.stop_time = None
 
     def start_parse(self, line):
-        time = '5'
-        self.start_time = int(time)
+        time = time_matcher.search(line).group()
+        self.start_time = time
 
     def end_parse(self, line):
-        time = '6'
-        self.stop_time = int(time)
-        self.store_record()
+        time = time_matcher.search(line).group()
+        self.stop_time = time
+        return self.store_record
+
+    def set_adds(self, line):
+        self.add_number = self.adds_matcher.search(line).group(1)
 
     def reset_fields(self):
+        self.crate = None
         self.start_time = None
         self.stop_time = None
+        self.add_number = None
+
+    def get_record(self):
+        return (
+            self.pallet,
+            self.crate,
+            self.destination_coord_sys,
+            self.start_time,
+            self.stop_time,
+            seconds_from_one(self.stop_time) - seconds_from_one(self.start_time),
+            self.add_number)
 
     def __str__(self):
-        return '{},{},{}'.format(self.name, self.start_time, self.stop_time)
+        return '{},{},{},{},{},{},{}'.format(
+            self.pallet,
+            self.crate,
+            self.destination_coord_sys,
+            self.start_time,
+            self.stop_time,
+            seconds_from_one(self.stop_time) - seconds_from_one(self.start_time),
+            self.add_number)
 
 
 if __name__ == '__main__':
-    output_filename = os.path.normpath("output/parsed_lines.log")
-    log_file = os.path.normpath('5.0.3_add_all_forklift.log')
-    # testlines = [
-    #     "DEBUG   02-08 01:02:11       lift:   56 {   'destination': 'C:\\Scheduled\\staging\\economy.gdb\\EnterpriseZones'",
-    #     'INFO    02-08 01:01:02       core:  237 checking for changes...',
-    #     'empty',
-    #     'DEBUG   02-08 01:01:04       core:  147 Number of rows to be added: 1666',
-    #     'INFO    02-08 01:01:02       core:  237 checking for changes...',
-    #     'empty',
-    #     'DEBUG   02-08 01:01:04       core:  147 Number of rows to be added: 200'
-    #     ]
-    t = HashInsertTemp()
-    reproject = Reproject()
-    search_expressions = [t]#, reproject]
+    log_file = os.path.normpath('data/logs/forklift_2_13.log')
 
+    parsed_sections = [
+        HashInsertTemp(),
+        Reproject()]
     with open(log_file, 'r') as log:
             for line in log:
-                for s in search_expressions:
-                    m = s.matcher.search(line)
-                    if m is not None:
-                        s.call_match_functions(m.group(), line)
+                for substring in CrateParser.get_search_strings():
+                    if substring in line:
+                        CrateParser.call_match_functions(substring, line)
+                CrateParser.store_records()
 
-    with open(output_filename, 'w') as out_file:
-        out_file.write('pallet,dest,dest_coord_sys,start,end,added\n')
-        for rec in t.records:
-            out_file.write(rec + '\n')
+    for p in parsed_sections:
+        with open(os.path.join('data/output', p.output_filename), 'w') as out_file:
+            writer = csv.writer(out_file)
+            writer.writerow(p.output_fields)
+            writer.writerows(p.records)
